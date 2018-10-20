f75481b3fbe539a878285f0a557b240e2ffc82a1
Lino Oropeza <linooropea@gmail.com>
2018-10-19 23:39:44 -0400

    Adding some fumctionality to the Vector2D class

---
+++ lib/math/Vector2D.rb
@@ -0,0 +1,33 @@
+# Vector2D will handle a vector for easier posiitoning
+# of coordinates in the space
+class Vector2D
+  attr_accessor :x_coordinate, :y_coordinate
+
+  # The default value of a vector will be an empty
+  # vector
+  def initialize(x_coordinate = 0, y_coordinate = 0)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+  end
+
+  def +(another)
+    if another.is_a?(Vector2D)
+      Vector2D.new @x_coordinate + another.x_coordinate,
+                   @y_coordinate + another.y_coordinate
+    end
+  end
+
+  def -(another)
+    if another.is_a?(Vector2D)
+      Vector2D.new @x_coordinate - another.x_coordinate,
+                   @y_coordinate - another.y_coordinate
+    end
+  end
+
+  def ==(another)
+    if another.is_a?(Vector2D)
+      @x_coordinate == another.x_coordinate &&
+        @y_coordinate == another.y_coordinate ? true : false
+    end
+  end
+end
---
+++ math.rb
@@ -0,0 +1,4 @@
+# The module math will contain some math classes
+# needed for the development of the game
+module Math
+end
